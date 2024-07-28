3f59bcfbfe6568130a7b03ddcec3ee0cb550e94d
you4rin <artichoke42@naver.com>
2024-07-28 19:47:56 +0900

    change package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-transformers",
+    name: "swift-transformers-forked",
     platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         .library(name: "Transformers", targets: ["Tokenizers", "Generation", "Models"]),
